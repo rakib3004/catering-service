0b99224b536a7dc4f280da16f48833e06f251901
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-09-20 20:59:38 +0600

    complete cards

--- frontend/src/components/CateringServiceCard.jsx
+++ frontend/src/components/CateringServiceCard.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const CateringServiceCard = ({cateringService}) => {
   return (
-    <div></div>
+    <div>
+      <div className='h-12 w-12 rounded-full '>
+      <img src={cateringService.image} alt={cateringService.name}/>
+
+      </div>
+      <h1>{cateringService.name}</h1>
+      <h2>{cateringService.location}</h2>
+      <h2>Total Items{cateringService.items}</h2>
+
+    </div>
   )
 }
 
--- frontend/src/features/CateringServiceList.jsx
+++ frontend/src/features/CateringServiceList.jsx
@@ -4,61 +4,61 @@ import CateringServiceCard from '../components/CateringServiceCard'
 const CateringServiceList = () => {
     const cateringServices = [
         {
-            image: '',
+            image: 'catering/paragon.png',
             name: 'Paragon Catering Ltd.',
             location: 'Dhanmondi, Dhaka',
             items: '21'
         },
         {
-            image: '',
+            image: 'catering/hazi.png',
             name: 'Hazi Catering Ltd.',
             location: 'Merul Badda, Dhaka',
             items: '10'
         }, 
         {
-            image: '',
+            image: 'catering/alpha.png',
             name: 'Alpha Catering Service',
             location: 'Satmosjid Road, Dhaka',
             items: '25'
         }, 
         {
-            image: '',
+            image: 'catering/desh.png',
             name: 'Desh Catering Service',
             location: 'Jonson Road, Dhaka',
             items: '20'
         },
         {
-            image: '',
+            image: 'catering/salsa.png',
             name: 'Salsa Catering Service',
             location: 'Satarkul, Dhaka',
             items: '21'
         },
         {
-            image: '',
+            image: 'catering/iqbal.png',
             name: 'Iqbal Catering',
             location: 'Mohammadpur, Dhaka',
             items: '21'
         },
         {
-            image: '',
+            image: 'catering/monsoon.png',
             name: 'Monsoon Catering',
             location: 'Badda, Dhaka',
             items: '10'
         },
         {
-            image: '',
+            image: 'catering/palmera.png',
             name: 'Palmera Catering',
             location: 'Uttara, Dhaka',
             items: '25'
         },
         {
-            image: '',
+            image: 'catering/alomgir.png',
             name: 'Alomgir Catering Service',
             location: 'Khilgoan, Dhaka',
             items: '20'
         },
         {
-            image: '',
+            image: 'catering/gmt.png',
             name: 'G.M.T Catering Services',
             location: 'Banani, Dhaka',
             items: '21'
