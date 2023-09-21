ed367db8e5774b8ca0dc278ff42f9d84b49de7d1
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-09-21 17:32:34 +0600

    add food table

---
+++ frontend/src/components/FoodTable.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const FoodTable = () => {
+  const [foodItems, setFoodItems] = useState([
+    { id: 1, name: "Chichen Biriyani", code: "#412", price: "140 tk" },
+    { id: 2, name: "Kacchi", code: "#132", price: "180 tk" },
+    { id: 3, name: "Chichen Fried Rice", code: "#402", price: "140 tk" },
+    { id: 4, name: "Beef Kichuri", code: "#232", price: "210 tk" },
+    { id: 5, name: "Chichen Kichuri", code: "#112", price: "140 tk" },
+    { id: 6, name: "Vorta Vaat", code: "#172", price: "70 tk" },
+  ]);
+
+  const removeFoodItem = (id) => {
+    setFoodItems(foodItems.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="container mx-auto">
+      <table className="min-w-full table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Food Name</th>
+            <th className="px-4 py-2">Food Code</th>
+            <th className="px-4 py-2">Price</th>
+            <th className="px-4 py-2">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {foodItems.map((item) => (
+            <tr key={item.id} className={item.id % 2 === 0 ? "bg-white" : "bg-gray-200"}>
+              <td className="border px-4 py-2 text-center">{item.name}</td>
+              <td className="border px-4 py-2 text-center">{item.code}</td>
+              <td className="border px-4 py-2 text-center">{item.price}</td>
+              <td className="border px-4 py-2 text-center">
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-base py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                  onClick={() => removeFoodItem(item.id)}
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FoodTable;
--- frontend/src/pages/CartDetails.jsx
+++ frontend/src/pages/CartDetails.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import FoodTable from '../components/FoodTable'
 
 const CartDetails = () => {
   return (
-    <div>CartDetails</div>
+    <div>
+      <div className='flex flex-row p-6'>
+        <h1 className='mx-6 text-3xl font-bold'>Cart Details</h1>
+        <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline'>Remove All</button>
+      </div>
+      <FoodTable/>
+      <div>
+        <div className='flex justify-center my-7 py-3'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white text-2xl font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline'>Review Subscription</button>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
